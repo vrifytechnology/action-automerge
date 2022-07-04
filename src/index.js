cb0a628824630de64d9aaa13644a99cb2ce5c289
jamesnakagawa <me@james.computer>
2022-07-04 16:51:14 -0700

    Update messages and formatting

--- src/index.js
+++ src/index.js
@@ -7,53 +7,29 @@ const token = core.getInput('github_token')
 const octokit = new Octokit({ auth: token })
 const repo = github.context.repo
 
-function slackSuccessMessage(source, target, status) {
-  return {
-      color: "#27ae60",
-      icon: ":white_check_mark:",
-      message: `${source} was successfully merged into ${target}.`,
-      description: `*${target}* can be pushed to production!`
-  }
-}
-
-function slackErrorMessage(source, target, status) {
-  return {
-      color: "#C0392A",
-      icon: ":red_circle:",
-      message: `*${source}* has confilct with *${target}*.`,
-      description: ":face_with_head_bandage: Fix me please :pray:"
-  }
-}
-
-async function slackMessage(source, target, status) {
-  if (core.getInput('webhook_url')) {
-    let payload = status == 'success' ?
-                  slackSuccessMessage(source, target, status) :
-                  slackErrorMessage(source, target, status)
-
-    slack.send({
-      icon_emoji: payload.icon,
-      username: payload.message,
-      attachments: [
-          {
-              author_name: github.context.payload.repository.full_name,
-              author_link: `https://github.com/${github.context.payload.repository.full_name}/`,
-              title: payload.message,
-              text: payload.description,
-              color: payload.color,
-              fields: [
-                  { title: 'Job Status', value: status, short: false },
-              ],
-          },
-      ],
-    });
-  }
+async function slackFailedMessage(source, target) {
+  if (!core.getInput('webhook_url')) return;
+  slack.send({
+    icon_emoji: ":red_circle:",
+    username: `*${source}* has a merge conflict with *${target}*.`,
+    attachments: [
+        {
+            author_name: github.context.payload.repository.full_name,
+            author_link: `https://github.com/${github.context.payload.repository.full_name}/`,
+            text: `*${source}* has a merge conflict with *${target}*.`,
+            color: "#C0392A",
+            fields: [
+                { title: 'Job Status', value: 'failed', short: false },
+            ],
+        },
+    ],
+  });
 }
 
 async function merge(source, target) {
   core.info(`merge branch:${source} to: ${target}`)
 
-  const response = await octokit.repos.merge({
+  await octokit.repos.merge({
     owner: repo.owner,
     repo: repo.repo,
     base: target,
@@ -69,12 +45,10 @@ async function run() {
 
   try {
     await merge(source, target)
-    await slackMessage(source, target, 'success')
   } catch (error) {
-    await slackMessage(source, target, 'failure')
+    await slackFailedMessage(source, target)
     core.setFailed(`${source} merge failed: ${error.message}`)
   }
-
 }
 
 run()
