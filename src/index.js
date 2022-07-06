5a781b213e5f152163c99c44b956c4bebbc92b2a
jamesnakagawa <me@james.computer>
2022-07-06 09:04:49 -0700

    v1.2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ const slack = require('slack-notify')(core.getInput('webhook_url'));
 
 const token = core.getInput('github_token')
 const octokit = new Octokit({ auth: token })
-const repo = github.context.repo
+const { repo, payload: { pull_request, workflow_run } } = github.context
+
+function capitalize(str) {
+  return str[0].toUpperCase() + str.split('').slice(1).join('')
+}
 
 async function slackFailedMessage(source, target) {
   if (!core.getInput('webhook_url')) return;
@@ -15,11 +19,13 @@ async function slackFailedMessage(source, target) {
     attachments: [
         {
             author_name: github.context.payload.repository.full_name,
-            author_link: `https://github.com/${github.context.payload.repository.full_name}/`,
-            text: `*${source}* has a merge conflict with *${target}*.`,
-            color: "#C0392A",
+            author_link: workflow_run.url,
+            title: `*${capitalize(source)}* has a merge conflict with *${target}*.`,
+            text: `"${pull_request.title}" has conflicts with ${target} that must be resolved.`,
             fields: [
-                { title: 'Job Status', value: 'failed', short: false },
+                { title: 'Merge Status', value: 'failed', short: false },
+                { title: 'PR Number', value: pull_request.number, short: false },
+                { title: 'Author', value: pull_request.user.login, short: false },
             ],
         },
     ],
@@ -39,7 +45,7 @@ async function merge(source, target) {
 }
 
 async function run() {
-  core.info(`version: 1.1.0`)
+  core.info(`version: 1.2.0`)
 
   const source = core.getInput('source')
   const target = core.getInput('target')
